af027576a53c846c05a1abaf5f12ecdce024eabc
Venkatesan Mahalingam <venkatesan_mahalinga@dell.com>
2020-05-06 15:17:02 -0700

    SNC-5365 - BGP neighbor level validations are done only when the NBR_TABLE is present in the DB

--- translib/transformer/xfmr_bgp_nbr.go
+++ translib/transformer/xfmr_bgp_nbr.go
@@ -283,23 +283,20 @@ var YangToDb_bgp_nbr_asn_fld_xfmr FieldXfmrYangToDb = func(inParams XfmrParams) 
     neigh_key := db.Key{Comp: []string{vrf, pNbrAddr}}
 
     entryValue, err := configDbAdd.GetEntry(nbrCfgTblTs, neigh_key)
-    if err != nil {
-        log.Info("YangToDb_bgp_nbr_peer_type_fld_xfmr: entry not found for key ", neigh_key)
-        return res_map, err
-    }
-    neigh_field := entryValue.Field;
-
-    if value, ok := neigh_field["peer_type"] ; ok {
-        err = errors.New("Can't specify  ASN as BGP neighbor as peer type is set to " + value);
-        return res_map, err
+    if err == nil {
+        neigh_field := entryValue.Field;
+        if value, ok := neigh_field["peer_type"] ; ok {
+            err = errors.New("Can't specify  ASN as BGP neighbor as peer type is set to " + value);
+            return res_map, err
+        }
     }
 
     asn_no, _ := inParams.param.(*uint32)
 
-    log.Info("YangToDb_bgp_nbr_peer_type_fld_xfmr: ", inParams.ygRoot, " Xpath: ", inParams.uri, " asn : ", *asn_no)
+    log.Info("YangToDb_bgp_nbr_asn_fld_xfmr: ", inParams.ygRoot, " Xpath: ", inParams.uri, " asn : ", *asn_no)
 
     res_map["asn"] = strconv.FormatUint(uint64(*asn_no), 10)
-    return res_map, err
+    return res_map, nil
 }
 
 var DbToYang_bgp_nbr_asn_fld_xfmr FieldXfmrDbtoYang = func(inParams XfmrParams) (map[string]interface{}, error) {
@@ -357,15 +354,13 @@ var YangToDb_bgp_nbr_peer_type_fld_xfmr FieldXfmrYangToDb = func(inParams XfmrPa
     neigh_key := db.Key{Comp: []string{vrf, pNbrAddr}}
     
     entryValue, err := configDbAdd.GetEntry(nbrCfgTblTs, neigh_key) 
-    if err != nil {
-        log.Info("YangToDb_bgp_nbr_peer_type_fld_xfmr: entry not found for key ", neigh_key)
-        return res_map, err
-    }
-    /* Either ASN or peer_type can be configured , not both */
-    neigh_field := entryValue.Field;
-    if value, ok := neigh_field["asn"] ; ok {
-        err = errors.New("Can't specify  peer type as BGP neighbor as ASN is set to " + value);
-        return res_map, err
+    if err == nil {
+        /* Either ASN or peer_type can be configured , not both */
+        neigh_field := entryValue.Field
+        if value, ok := neigh_field["asn"] ; ok {
+            err = errors.New("Can't specify  peer type as BGP neighbor as ASN is set to " + value);
+            return res_map, err
+        }
     }
     
     if (peer_type == ocbinds.OpenconfigBgp_PeerType_INTERNAL) {
@@ -1612,7 +1607,10 @@ var YangToDb_bgp_nbr_community_type_fld_xfmr FieldXfmrYangToDb = func(inParams X
     }
 
     community_type, _ := inParams.param.(ocbinds.E_OpenconfigBgpExt_BgpExtCommunityType)
-    log.Info("YangToDb_bgp_nbr_community_type_fld_xfmr: ", inParams.ygRoot, " Xpath: ", inParams.uri, " community_type: ", community_type)
+    community_type2, _ := inParams.param.(*uint16)
+    community_type3, _ := inParams.param.(*string)
+    log.Info("YangToDb_bgp_nbr_community_type_fld_xfmr: ", inParams.ygRoot, " Xpath: ", inParams.uri, 
+             " community_type: ", community_type, "uint16 ", community_type2, "string ", community_type3)
 
     if (community_type == ocbinds.OpenconfigBgpExt_BgpExtCommunityType_STANDARD) {
         res_map["send_community"] = "standard"
