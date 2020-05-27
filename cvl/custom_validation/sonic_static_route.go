736063426284c5b0904ba6d3919f4121dcb096d8
Zhenhong Zhao <Zhenhong_Zhao@dell.com>
2020-05-26 20:12:39 -0700

    Added NH attribute alignment check with DB data

--- cvl/custom_validation/sonic_static_route.go
+++ cvl/custom_validation/sonic_static_route.go
@@ -2,48 +2,97 @@ package custom_validation
 
 import (
     "github.com/go-redis/redis/v7"
+    util "github.com/Azure/sonic-mgmt-common/cvl/internal/util"
     "fmt"
     "net"
     "strings"
 )
 
-func getNexthopAttrList(vc *CustValidationCtxt) (string, []string, error) {
+func checkAttrAlignment(vc *CustValidationCtxt) (string, error) {
     keys := strings.Split(vc.CurCfg.Key, "|")
     if len(keys) < 2 || keys[0] != "STATIC_ROUTE" {
-        return "", nil, fmt.Errorf("Invalid key format: %s", vc.CurCfg.Key)
+        return "", fmt.Errorf("Invalid key format: %s", vc.CurCfg.Key)
     }
     prefix := keys[len(keys) - 1]
-    cfgAttrVal, ok := vc.CurCfg.Data[vc.YNodeName]
-    if vc.CurCfg.VOp == OP_DELETE || !ok || len(cfgAttrVal) == 0 {
-        return prefix, []string{}, nil
-    }
     if vc.SessCache.Data == nil {
-        vc.SessCache.Data = make(map[string]int)
+        vc.SessCache.Data = make(map[string]bool)
     }
-    nhNumMap, ok := vc.SessCache.Data.(map[string]int)
+    nhCheckMap, ok := vc.SessCache.Data.(map[string]bool)
     if !ok {
-        return "", nil, fmt.Errorf("Invalid data type in session cache")
+        return "", fmt.Errorf("Invalid data type in session cache")
     }
-    vals := strings.Split(cfgAttrVal, ",")
-    cfgNhNum, ok := nhNumMap[prefix]
-    if !ok {
+    if _, ok := nhCheckMap[prefix]; !ok {
         // verify attr alignment and get list item number
-        cfgNhNum = 0
+        cfgNhNum := 0
         for _, fv := range vc.CurCfg.Data {
             if cfgNhNum == 0 {
                 cfgNhNum = len(strings.Split(fv, ","))
             } else if cfgNhNum != len(strings.Split(fv, ",")) {
-                return "", nil, fmt.Errorf("NH config attributes not aligned")
+                return "", fmt.Errorf("NH config attributes not aligned")
             }
         }
-        nhNumMap[prefix] = cfgNhNum
+        // check DB data alignment
+        attrs, err := vc.RClient.HGetAll(vc.CurCfg.Key).Result()
+	    if err != nil && err != redis.Nil {
+            return "", fmt.Errorf("Failed to read NH attribute from DB, key: %s", vc.CurCfg.Key)
+        }
+        dbFields := make(map[string]bool)
+        var dbNhNum int
+	    if err != redis.Nil && len(attrs) != 0 {
+            for dfn, dfv := range attrs {
+                itemNum := len(strings.Split(dfv, ","))
+                if dbNhNum == 0 {
+                    dbNhNum = itemNum
+                } else if dbNhNum != itemNum {
+                    util.CVL_LEVEL_LOG(util.INFO, "Original DB data is not aligned, bypass checking")
+                    return prefix, nil
+                }
+                dbFields[dfn] = true
+            }
+        }
+        if cfgNhNum != dbNhNum {
+            // check if update covered all non-delete fields
+            for _, cfgData := range vc.ReqData {
+                if cfgData.VOp == OP_DELETE {
+                    if cfgData.Data == nil || len(cfgData.Data) == 0 {
+                        return prefix, nil
+                    }
+                    for fn, _ := range cfgData.Data {
+                        if _, ok := dbFields[fn]; ok {
+                            delete(dbFields, fn)
+                        }
+                    }
+                }
+            }
+            for fn, _ := range dbFields {
+                if _, ok := vc.CurCfg.Data[fn]; !ok {
+                    return "", fmt.Errorf("Field %s in DB need to be updated to keep alignment");
+                }
+            }
+        }
+        nhCheckMap[prefix] = true
     }
-    if cfgNhNum != len(vals) {
-        return "", nil, fmt.Errorf("Given attr number %d is not aligned to existing NH number %d",
-                                   len(vals), cfgNhNum)
-    }
+    return prefix, nil
+}
 
-    return prefix, vals, nil
+type checkNHAttrHdlr func(string, string, ...interface{}) error
+
+func checkNexthopGateway(prefix string, gwIP string, args ...interface{}) error {
+    pfxIpStr := strings.Split(prefix, "/")[0]
+    pfxIp := net.ParseIP(pfxIpStr)
+    if pfxIp == nil {
+        return fmt.Errorf("Invalid static route IP prefix: %s", prefix)
+    }
+    pfxIpv4 := pfxIp.To4() != nil
+    ip := net.ParseIP(gwIP)
+    if ip == nil {
+        return fmt.Errorf("Invalid gateway IP format %s", gwIP)
+    }
+    gwIpv4 := ip.To4() != nil
+    if gwIpv4 != pfxIpv4 {
+        return fmt.Errorf("Address family of NH gateway %s not same as prefix %s", gwIP, pfxIpStr)
+    }
+    return nil
 }
 
 func checkTableKeyExists(db *redis.Client, tableList []string, key string) bool {
@@ -57,36 +106,45 @@ func checkTableKeyExists(db *redis.Client, tableList []string, key string) bool 
     return false
 }
 
-//Path : /sonic-static-route/STATIC_ROUTE/nexthop
-// Purpose: To check if every item in comma separated list is valid IP address
-// Returns -  CVL Error object
-func (t *CustomValidation) ValidateNexthopGateway(
-	vc *CustValidationCtxt) CVLErrorInfo {
-    prefix, gwIpList, err := getNexthopAttrList(vc)
+func checkNexthopIntfVrf(_, name string, args ...interface{}) error {
+    if len(args) < 2 {
+        return fmt.Errorf("No enough arguments given for NH interface or VRF check")
+    }
+    if len(name) == 0 {
+        return nil
+    }
+    tableList, ok := args[0].([]string)
+    if !ok {
+        return fmt.Errorf("Invalid argument type of table list for NH interface or VRF check")
+    }
+    db, ok := args[1].(*redis.Client)
+    if !ok {
+        return fmt.Errorf("Invalid argument type of DB client for NH interface or VRF check")
+    }
+    if found := checkTableKeyExists(db, tableList, name); !found {
+        return fmt.Errorf("Interface or VRF %s not found in config DB", name)
+    }
+    return nil
+}
+
+func validateNexthopAttrCmn(vc *CustValidationCtxt, hdlr checkNHAttrHdlr,
+                            args ...interface{}) CVLErrorInfo {
+    if vc.CurCfg.VOp == OP_DELETE || len(vc.YNodeVal) == 0 {
+	    return CVLErrorInfo{ErrCode: CVL_SUCCESS}
+    }
+    util.CVL_LEVEL_LOG(util.INFO, "Nexthop attribute validation: Oper %d Key %s Node %s Value %s",
+                       vc.CurCfg.VOp, vc.CurCfg.Key, vc.YNodeName, vc.YNodeVal)
+    prefix, err := checkAttrAlignment(vc)
     if err != nil {
+        errMsg := fmt.Sprintf("Failed checking alignment: %s", err)
         return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                            Msg: err.Error()}
+                            Msg: errMsg}
     }
-    if len(gwIpList) == 0 {
-        return CVLErrorInfo{ErrCode: CVL_SUCCESS}
-    }
-    pfxIpStr := strings.Split(prefix, "/")[0]
-    pfxIp := net.ParseIP(pfxIpStr)
-    if pfxIp == nil {
-        return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                            Msg: "Invalid static route IP prefix"}
-    }
-    pfxIpv4 := pfxIp.To4() != nil
-    for _, gwIP := range gwIpList {
-        ip := net.ParseIP(gwIP)
-        if ip == nil {
-            errMsg := fmt.Sprintf("Invalid gateway IP format %s", gwIP)
-            return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                                Msg: errMsg}
-        }
-        gwIpv4 := ip.To4() != nil
-        if gwIpv4 != pfxIpv4 {
-            errMsg := fmt.Sprintf("Address family of NH gateway %s not same as prefix %s", gwIP, pfxIpStr)
+    attrs := strings.Split(vc.YNodeVal, ",")
+    for _, attrVal := range attrs {
+        err = hdlr(prefix, attrVal, args...)
+        if err != nil {
+            errMsg := fmt.Sprintf("Validate failed for attribute %s", attrVal)
             return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
                                 Msg: errMsg}
         }
@@ -94,28 +152,21 @@ func (t *CustomValidation) ValidateNexthopGateway(
     return CVLErrorInfo{ErrCode: CVL_SUCCESS}
 }
 
+//Path : /sonic-static-route/STATIC_ROUTE/nexthop
+// Purpose: To check if every item in comma separated list is valid IP address
+// Returns -  CVL Error object
+func (t *CustomValidation) ValidateNexthopGateway(
+	vc *CustValidationCtxt) CVLErrorInfo {
+    return validateNexthopAttrCmn(vc, checkNexthopGateway)
+}
+
 //Path : /sonic-static-route/STATIC_ROUTE/ifname
 // Purpose: To check if every item in comma separated list is an active interface name
 // Returns -  CVL Error object
 func (t *CustomValidation) ValidateNexthopInterface(
 	vc *CustValidationCtxt) CVLErrorInfo {
-    _, intfList, err := getNexthopAttrList(vc)
-    if err != nil {
-        return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                            Msg: err.Error()}
-    }
     var tableList = []string{"PORT", "PORTCHANNEL", "VLAN", "LOOPBACK_INTERFACE"}
-    for _, ifName := range intfList {
-        if len(ifName) == 0 {
-            continue
-        }
-        if found := checkTableKeyExists(vc.RClient, tableList, ifName); !found {
-            errMsg := fmt.Sprintf("Interface %s not found in config DB", ifName)
-            return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                                Msg: errMsg}
-        }
-    }
-	return CVLErrorInfo{ErrCode: CVL_SUCCESS}
+    return validateNexthopAttrCmn(vc, checkNexthopIntfVrf, tableList, vc.RClient)
 }
 
 //Path : /sonic-static-route/STATIC_ROUTE/nexthop-vrf
@@ -123,22 +174,6 @@ func (t *CustomValidation) ValidateNexthopInterface(
 // Returns -  CVL Error object
 func (t *CustomValidation) ValidateNexthopVrf(
 	vc *CustValidationCtxt) CVLErrorInfo {
-	return CVLErrorInfo{ErrCode: CVL_SUCCESS}
-    _, vrfList, err := getNexthopAttrList(vc)
-    if err != nil {
-        return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                            Msg: err.Error()}
-    }
     var tableList = []string{"VRF"}
-    for _, vrfName := range vrfList {
-        if len(vrfName) == 0 {
-            continue
-        }
-        if found := checkTableKeyExists(vc.RClient, tableList, vrfName); !found {
-            errMsg := fmt.Sprintf("VRF %s not found in config DB", vrfName)
-            return CVLErrorInfo{ErrCode: CVL_ERROR, Keys:[]string{vc.CurCfg.Key}, Value: vc.YNodeVal, Field: vc.YNodeName,
-                                Msg: errMsg}
-        }
-    }
-	return CVLErrorInfo{ErrCode: CVL_SUCCESS}
+    return validateNexthopAttrCmn(vc, checkNexthopIntfVrf, tableList, vc.RClient)
 }
