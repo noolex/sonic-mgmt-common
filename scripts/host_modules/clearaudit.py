a5ce6983b85dcce083eee40822d26e6cf330d42b
Srinadh Penugonda <Srinadh_Penugondaa@dell.com>
2020-06-10 11:00:11 -0700

    fix intialization issue

--- scripts/host_modules/clearaudit.py
+++ scripts/host_modules/clearaudit.py
@@ -10,7 +10,7 @@ class ClearAudit(host_service.HostModule):
     """DBus endpoint that executes the "rm /var/log/audit.log and service rsyslog restart" commands
     """
     @host_service.method(host_service.bus_name(MOD_NAME))
-    def action():
+    def action(self):
 
         print("Host side: Clearing audit log")
         rmcmd = ['/bin/rm -rf /var/log/audit.log']
