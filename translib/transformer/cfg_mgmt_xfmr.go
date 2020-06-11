f6d296a275b43e157989d48a23b825dec671b7e6
Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
2020-06-11 08:58:59 -0700

    [JIRA SONIC-21502] Fix go static analysis warnings in cfg_mgmt_xfmr.go
    Change-Id: I079ec8afe744350a493a1e5ff72d55b0aac63459

--- translib/transformer/cfg_mgmt_xfmr.go
+++ translib/transformer/cfg_mgmt_xfmr.go
@@ -19,13 +19,12 @@
 package transformer
 
 import (
-     "errors"
-     "fmt"
-    "strings"
-	"encoding/json"
-	"github.com/Azure/sonic-mgmt-common/translib/db"
-	log "github.com/golang/glog"
-    "github.com/golang/glog"
+        "errors"
+        "fmt"
+        "strings"
+        log "github.com/golang/glog"
+        "encoding/json"
+        "github.com/Azure/sonic-mgmt-common/translib/db"
 )
 
 func init() {
@@ -62,8 +61,8 @@ func init() {
 
 func validate_filename(filename string) (fname string, err error ) {
    
-    if  (strings.HasPrefix(filename, "file://etc/sonic/") == false)  ||
-        (strings.Contains(filename, "/..") == true)  {
+    if  (!strings.HasPrefix(filename, "file://etc/sonic/"))  ||
+        (strings.Contains(filename, "/.."))  {
             return filename, errors.New("ERROR:Invalid filename " + filename)
     }
 
@@ -118,7 +117,7 @@ func cfg_copy_action(body []byte) ([]byte, error) {
        if (err == nil ) {
             config := Config{source, destination, operand.Input.Overwrite}
             cfg_cmd, ok := cfg_op_map[config]
-            if ok == true {
+            if ok {
                if (source == "filename")  ||
                    (destination == "filename") {
                        options = append(options, filename)
@@ -138,16 +137,16 @@ func cfg_copy_action(body []byte) ([]byte, error) {
     } else if query_result.Err != nil {
         sum.Output.Status_detail = "ERROR:Internal SONiC Hostservice communication failure."
     } else if query_result.Body[0].(int32) ==2 {
-            sum.Output.Status_detail = fmt.Sprintf("ERROR:Invalid filename %s.", filename)
+        sum.Output.Status_detail = fmt.Sprintf("ERROR:Invalid filename %s.", filename)
     } else if query_result.Body[0].(int32) != 0 {
-            sum.Output.Status_detail = "ERROR:Command Failed."
+        sum.Output.Status_detail = "ERROR:Command Failed."
     } else {
-            sum.Output.Status = 0
-            sum.Output.Status_detail = "SUCCESS."
+        sum.Output.Status = 0
+        sum.Output.Status_detail = "SUCCESS."
     }
     result, err = json.Marshal(&sum)
 
-	return result, err
+    return result, err
 }
 
 func cfg_write_erase_action(body []byte) ([]byte, error) {
@@ -156,27 +155,28 @@ func cfg_write_erase_action(body []byte) ([]byte, error) {
     var subcmd string
 
     var operand struct {
-		Input struct {
-			SubCmd string `json:"subcmd"`
-		} `json:"sonic-config-mgmt:input"`
-	}
+	Input struct {
+		SubCmd string `json:"subcmd"`
+	} `json:"sonic-config-mgmt:input"`
+    }
 
     err = json.Unmarshal(body, &operand)
-	if err != nil {
+    if err != nil {
         /* Unmarshall failed, no input provided.
          * set to default */
        log.Error("Config input not provided.")
        err = errors.New("Input parameters missing.")
-	} else {
+       return nil, err
+    } else {
        subcmd = operand.Input.SubCmd
     }
 
     var sum struct {
-		Output struct {
-			Status int32 `json:"status"`
+        Output struct {
+	    Status int32 `json:"status"`
             Status_detail string`json:"status-detail"`
-		} `json:"sonic-config-mgmt:output"`
-	}
+	} `json:"sonic-config-mgmt:output"`
+    }
 
     var fcnt string = "cfg_mgmt.write_erase"
     var option string = "?"
@@ -195,8 +195,8 @@ func cfg_write_erase_action(body []byte) ([]byte, error) {
 
     host_output := HostQuery(fcnt, option)
     if host_output.Err != nil {
-        glog.Errorf("host Query failed: err=%v", host_output.Err)
-        glog.Flush()
+        log.Errorf("host Query failed: err=%v", host_output.Err)
+        log.Flush()
         return nil, host_output.Err
     }
 
@@ -207,12 +207,11 @@ func cfg_write_erase_action(body []byte) ([]byte, error) {
         sum.Output.Status_detail = host_output.Body[1].(string)
     }
 
-	result, err = json.Marshal(&sum)
+    result, err = json.Marshal(&sum)
 
-	return result, err
+    return result, err
 }
 
 var rpc_write_erase RpcCallpoint = func(body []byte, dbs [db.MaxDB]*db.DB) ([]byte, error) {
     return cfg_write_erase_action(body)
 }
-
