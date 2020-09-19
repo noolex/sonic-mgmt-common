36ea8fa2eb06522bb6d0b5f7f8da9c2d7538faee
Jeff Yin <29264773+jeff-yin@users.noreply.github.com>
2020-09-19 14:15:56 -0700

    Resolve merge conflict for platform_config.go
    Signed-off-by: Jeff Yin <29264773+jeff-yin@users.noreply.github.com>

--- translib/transformer/platform_config.go
+++ translib/transformer/platform_config.go
@@ -300,52 +300,6 @@ func parsePlatformDefJsonFile () (error) {
         return err
     }
 
-<<<<<<< HEAD
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDef4Level)
-||||||| merged common ancestors
-<<<<<<< Temporary merge branch 1
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDef4Level)
-||||||| merged common ancestors
-<<<<<<< Temporary merge branch 1
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDef4Level)
-||||||| merged common ancestors
-<<<<<<< Temporary merge branch 1
-    platDefStr = make(map[string]map[string]map[string]string)
-    err = json.Unmarshal([]byte(file), &platDefStr)
-    log.Info(platDefStr)
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDefStr)
-||||||| merged common ancestors
-    platDefStr = make(map[string]map[string]map[string]string)
-    err = json.Unmarshal([]byte(file), &platDefStr)
-    log.Info(platDefStr)
-=======
-=======
-    //platDefStr = make(map[string]map[string]map[string]string)
-    //err = json.Unmarshal([]byte(file), &platDefStr)
-    //log.Info(platDefStr)
-    //platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    //json.Unmarshal([]byte(file), &platDef4Level)
-    //log.Info(platDef4Level)
-
->>>>>>> Temporary merge branch 2
-=======
-    //platDefStr = make(map[string]map[string]map[string]string)
-    //err = json.Unmarshal([]byte(file), &platDefStr)
-    //log.Info(platDefStr)
-    //platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    //json.Unmarshal([]byte(file), &platDef4Level)
-    //log.Info(platDef4Level)
-
->>>>>>> Temporary merge branch 2
-=======
     platDef4Level = make(map[string]map[string]map[string]map[string]string)
     json.Unmarshal([]byte(file), &platDef4Level)
     log.Info(platDef4Level)
@@ -356,7 +310,6 @@ func parsePlatformDefJsonFile () (error) {
     //json.Unmarshal([]byte(file), &platDef4Level)
     //log.Info(platDef4Level)
 
->>>>>>> origin/broadcom_sonic_3.x_share
     var fec_raw_map map[string]map[string]map[string]interface{}
 
     /* Map if for FEC parsing */
@@ -400,36 +353,6 @@ func parsePlatformDefJsonFile () (error) {
     } else {
         log.Info("No port-group configs to parse in platform-def")
     }
-<<<<<<< HEAD
-||||||| merged common ancestors
-<<<<<<< Temporary merge branch 1
-||||||| merged common ancestors
-<<<<<<< Temporary merge branch 1
-||||||| merged common ancestors
->>>>>>> Temporary merge branch 2
-=======
-
-/* Keeping this commented for now 
-    platDefStr = make(map[string]map[string]map[string]string)
-    err = json.Unmarshal([]byte(file), &platDefStr)
-    log.Info(platDefStr)
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDefStr)
-*/
->>>>>>> Temporary merge branch 2
-=======
-
-/* Keeping this commented for now 
-    platDefStr = make(map[string]map[string]map[string]string)
-    err = json.Unmarshal([]byte(file), &platDefStr)
-    log.Info(platDefStr)
-    platDef4Level = make(map[string]map[string]map[string]map[string]string)
-    json.Unmarshal([]byte(file), &platDef4Level)
-    log.Info(platDefStr)
-*/
->>>>>>> Temporary merge branch 2
-=======
 
     if vspeedEntries, ok := fec_raw_map["native-port-supported-speeds"]; ok {
         parseNativeValidSpeed(vspeedEntries)
@@ -443,7 +366,6 @@ func parsePlatformDefJsonFile () (error) {
     json.Unmarshal([]byte(file), &platDef4Level)
     log.Info(platDefStr)
 */
->>>>>>> origin/broadcom_sonic_3.x_share
     return err
 }
 
