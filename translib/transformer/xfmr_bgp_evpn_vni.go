bc487aceb1e92534bce39aa37a3c31ae669641e8
pvattem <phaniraj_vattem@dell.com>
2020-10-08 15:15:17 -0700

    log level changes

--- translib/transformer/xfmr_bgp_evpn_vni.go
+++ translib/transformer/xfmr_bgp_evpn_vni.go
@@ -492,10 +492,14 @@ var DbToYang_bgp_advertise_fld_xfmr FieldXfmrDbtoYang = func(inParams XfmrParams
     log.Info(inParams.key)
 
     data := (*inParams.dbDataMap)[inParams.curDb]
-    log.V(3).Info("DbToYang_bgp_advertise_fld_xfmr : ", data, "inParams : ", inParams)
+    if log.V(3) {
+        log.Info("DbToYang_bgp_advertise_fld_xfmr : ", data, "inParams : ", inParams)
+    }
 
     pTbl := data["BGP_GLOBALS_AF"]
-    log.V(3).Info("Table: ", pTbl)
+    if log.V(3) {
+        log.Info("Table: ", pTbl)
+    }
 
     if _, ok := pTbl[inParams.key]; !ok {
         log.Info("DbToYang_bgp_advertise_fld_xfmr BGP AF not found : ", inParams.key)
