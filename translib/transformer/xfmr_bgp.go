51d80e827c989bc651a78e30e9e424edf72bf9a0
Selvakumar Balu <sbalu@xenlogin-eqx-05.force10networks.com>
2020-10-28 01:39:53 -0700

    SNC_9252 and SNC_3593 cases handled

--- translib/transformer/xfmr_bgp.go
+++ translib/transformer/xfmr_bgp.go
@@ -242,6 +242,141 @@ func hdl_del_post_xfmr(inParams *XfmrParams, data *map[string]map[string]db.Valu
     return err
 }
 
+func hdl_validate_values_post_xfmr (inParams *XfmrParams) (error) {
+
+    var retVal error
+    retVal = validate_neighbor_post_xfmr(inParams)
+    if (retVal != nil) {
+       return retVal
+    }
+    retVal = validate_peer_group_post_xfmr(inParams)
+    if (retVal != nil) {
+       return retVal
+    }
+    return nil
+}
+
+func validate_neighbor_post_xfmr(inParams *XfmrParams) (error) {
+    var retVal  error
+    retVal = validate_prefix_limit_post_xfmr(inParams,"BGP_NEIGHBOR_AF")
+    if (retVal != nil) {
+       return retVal
+    }
+
+    retVal  = validate_fields_value_post_xfmr (inParams,"BGP_NEIGHBOR","ebgp_multihop_ttl","ttl_security_hops")
+    if (retVal != nil) {
+       return retVal
+    }
+
+    return nil
+}
+
+func validate_peer_group_post_xfmr (inParams *XfmrParams) (error) {
+
+    var retVal error
+    retVal = validate_prefix_limit_post_xfmr(inParams,"BGP_PEER_GROUP_AF")
+    if (retVal != nil) {
+       return retVal
+    }
+
+    retVal  = validate_fields_value_post_xfmr (inParams,"BGP_PEER_GROUP","ebgp_multihop_ttl","ttl_security_hops")
+    if (retVal != nil) {
+       return retVal
+    }
+    return nil
+}
+
+
+func validate_fields_value_post_xfmr (inParams *XfmrParams, tableName string, fieldName1 string, fieldName2 string) (error) {
+    var field1 int64
+    var field2 int64
+
+    if dataMapDB, ok := (*inParams.dbDataMap)[db.ConfigDB][tableName]; ok {
+        for key := range dataMapDB {
+            fieldVal1, ok1 := dataMapDB[key].Field[fieldName1]
+            fieldVal2, ok2 := dataMapDB[key].Field[fieldName2]
+            if ((ok1) && (ok2)) {
+                field1, _ =  strconv.ParseInt(fieldVal1, 10, 16)
+                field2, _ =  strconv.ParseInt(fieldVal2, 10, 16)
+                if ((field1 > 0 ) && (field2 > 0 )) {
+                   errStr := fieldName1 + " and " + fieldName2 +" cannot co-exist"
+                   log.Error(errStr)
+                   return tlerr.InvalidArgsError{Format:errStr}
+                }
+            } else {
+                tblSpec := &db.TableSpec{Name: tableName}
+                dbEntry, dbErr := inParams.d.GetEntry(tblSpec, db.Key{Comp: []string{key}})
+                if dbErr == nil {
+                    if ((!ok1) && (ok2)) {
+                        fieldValStr, ok  := dbEntry.Field[fieldName1]
+                        field1, _ :=  strconv.ParseInt(fieldValStr, 10, 16)
+                        field2, _ =  strconv.ParseInt(fieldVal2, 10, 16)
+                        if ok && ((field2 > 0 ) && (field1 >0)){
+                            errStr := fieldName2 + " is not supported with " + fieldName1
+                            log.Error(errStr)
+                            return  tlerr.InvalidArgsError{Format:errStr}
+                        }
+                    } else if ((ok1) && (!ok2)) {
+                        fieldValStr, ok  := dbEntry.Field[fieldName2]
+                        field2, _ :=  strconv.ParseInt(fieldValStr, 10, 16)
+                        field1, _ =  strconv.ParseInt(fieldVal1, 10, 16)
+                        if ok && ((field1 > 0 ) && (field2 >0)){
+                            errStr := fieldName1 + " is not supported with " + fieldName2
+                            log.Error(errStr)
+                            return tlerr.InvalidArgsError{Format:errStr}
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return nil
+}
+
+func validate_prefix_limit_post_xfmr(inParams *XfmrParams, tableName string) (error) {
+
+    if dataMapDB, ok := (*inParams.dbDataMap)[db.ConfigDB][tableName]; ok {
+        for key := range dataMapDB {
+            warn_str, warnOk := dataMapDB[key].Field["max_prefix_warning_only"]
+            restart_str, restartOk := dataMapDB[key].Field["max_prefix_restart_interval"]
+            if ((warnOk) && (restartOk)) {
+                restart_val, _ :=  strconv.ParseInt(restart_str, 10, 16)
+                if ((warn_str == "true") && (restart_val > 0 )) {
+                   errStr := "prevent-teardown and restart-timer are mutually exclusive"
+                   log.Error(errStr)
+                   return tlerr.InvalidArgsError{Format:errStr}
+                }
+            } else if ((!warnOk) && (restartOk)) {
+                tblSpec := &db.TableSpec{Name: tableName}
+                dbEntry, dbErr := inParams.d.GetEntry(tblSpec, db.Key{Comp: []string{key}})
+                if dbErr == nil {
+                    warningOnly, ok  := dbEntry.Field["max_prefix_warning_only"]
+                    if ok && warningOnly == "true" {
+                        //warning_only exists. Return error
+                        errStr := "restart-timer is not supported with prevent-teardown"
+                        log.Error(errStr)
+                        return  tlerr.InvalidArgsError{Format:errStr}
+                    }
+                }
+            } else if ((warnOk) && (!restartOk)) {
+                tblSpec := &db.TableSpec{Name: tableName}
+                dbEntry, dbErr := inParams.d.GetEntry(tblSpec, db.Key{Comp: []string{key}})
+                if dbErr == nil {
+                    _, ok  := dbEntry.Field["max_prefix_restart_interval"]
+                    if ok {
+                        //restart interval exists. Return error
+                        errStr := "prevent-teardown is not supported with restart-timer"
+                        log.Error(errStr)
+                        return tlerr.InvalidArgsError{Format:errStr}
+                    }
+                }
+            }
+        }
+    }
+    return nil
+}
+
+
 func bgp_hdl_post_xfmr(inParams *XfmrParams, data *map[string]map[string]db.Value) (error) {
     var err error
 
@@ -253,6 +388,11 @@ func bgp_hdl_post_xfmr(inParams *XfmrParams, data *map[string]map[string]db.Valu
     if inParams.oper == DELETE {
         err = hdl_del_post_xfmr(inParams, data)
         return err
+    } else {
+        retval := hdl_validate_values_post_xfmr(inParams)
+        if (retval != nil) {
+           return retval
+        }
     }
 
     tblName := "BGP_GLOBALS"
